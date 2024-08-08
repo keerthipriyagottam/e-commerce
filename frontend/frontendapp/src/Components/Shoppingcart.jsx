6effbcd6d950b8690792212d729a8d46fabefc44
Keerthi <keerthigottam0215@gmail.com>
2024-08-08 12:43:44 +0530

    shopping cart added

--- frontend/frontendapp/src/Components/Shoppingcart.jsx
+++ frontend/frontendapp/src/Components/Shoppingcart.jsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './shoppingcart.css'
 
-const Shoppingcart = () => {
+const Shoppingcart = (title,price,quantity) => {
+
   return (
     <>
     <div className='cart-main-container'>
-      <div className='cart-left'><img src='https://th.bing.com/th/id/OIP.RZnLzFsFh7tqQF_NqQlfEQAAAA?rs=1&pid=ImgDetMain' alt='main'/></div>
+      <div className='cart-left'>
+        <img src='https://th.bing.com/th/id/OIP.RZnLzFsFh7tqQF_NqQlfEQAAAA?rs=1&pid=ImgDetMain' alt='main'/>
+      </div>
       <div className='cart-right'>
-      <div className='cart-productname'>
-        Title
-      </div>
-      <div className='cart-price'>Price</div>
-      <div class="quantity-container">
-        <label>Quantity:</label>
-        <button class="quantity-button" id="decrement"> - </button>
-        <input type="number" id="quantity" value="1" min="1"/>
-        <button class="quantity-button" id="increment">+</button>
-        <div>
-        <button className='deletebutton'>Remove</button>
+        <div className='cart-productname'>Title</div>
+        <div className='cart-price'>Price</div>
+        <div class="quantity-container">
+          <label>Quantity:</label>
+          <button class="quantity-button" id="decrement" disabled={quantity < 1}> - </button>
+          <input type="number" id="quantity" value="1" min="1"/>
+          <button class="quantity-button" id="increment">+</button>
+          <div>
+            <button className='deletebutton'>Remove</button>
+          </div>
         </div>
-      </div>
-      <div className='cart-total'>Total</div>
-      <div className='payment'>Redirect to payment</div>
+        <div className='cart-total'>Total</div>
       </div>
     </div>
     
